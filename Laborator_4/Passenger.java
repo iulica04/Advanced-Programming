4c7572421004e12d5baf39d056b7eeada82fdf57
Vreme Iulia <126086021+iulica04@users.noreply.github.com>
2024-03-26 19:34:23 +0200

    Update Passenger.java

--- Laborator_4/Passenger.java
+++ Laborator_4/Passenger.java
@@ -1,15 +1,26 @@
 public class Passenger extends Person implements Comparable<Passenger> {
-    Passenger(String name, int age, String destination) {
+
+    String startLocation;
+
+    Passenger(String name, int age, String destination, String startLocation) {
         super(name, age, destination);
+        this.startLocation = startLocation;
     }
 
     public String getType() {
         return "Passenger";
     }
 
+    public String getStartLocation() {
+        return startLocation;
+    }
+
+    public void setStartLocation(String startLocation) {
+        this.startLocation = startLocation;
+    }
+
     @Override
     public int compareTo(Passenger otherPassenger) {
-        // Comparăm pasagerii după nume
         return this.getName().compareTo(otherPassenger.getName());
     }
 
@@ -17,7 +28,11 @@ public class Passenger extends Person implements Comparable<Passenger> {
     public String toString() {
         return "Name: " + getName() + ", " +
                 "Age: " + getAge() + ", " +
+                "Start location: " + getStartLocation() + ", " +
                 "Destination: " + getDestination() + ", " +
                 "Type: passenger" + "\n";
     }
+
+
 }
+
